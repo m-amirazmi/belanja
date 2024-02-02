a6e10d438e3e8ec08aa5b4f2414e05f8c9956dba
Muhamad Amir <m.amirazmi@hotmail.com>
2024-02-02 11:00:23 +0800

    WEB | Make the login page for mobile view

--- web-v02/src/components/layout-auth.tsx
+++ web-v02/src/components/layout-auth.tsx
@@ -3,12 +3,15 @@ import { Outlet } from "@tanstack/react-router";
 export default function LayoutAuth() {
   return (
     <div className="flex h-screen w-screen bg-slate-900">
-      <div className="flex-1 bg-slate-900 p-4">
+      <div className="hidden md:block flex-1 bg-slate-900 p-4">
         <span className="text-lg font-bold tracking-widest text-[#f8fafc]">
           BELANJA.
         </span>
       </div>
-      <div className="flex-1 bg-background">
+      <div className="flex-1 bg-background p-4 overflow-hidden md:overflow-auto">
+        <span className="md:hidden text-lg font-bold tracking-widest text-[#f8fafc]">
+          BELANJA.
+        </span>
         <div className="flex flex-col h-full items-center justify-center">
           <Outlet />
         </div>
